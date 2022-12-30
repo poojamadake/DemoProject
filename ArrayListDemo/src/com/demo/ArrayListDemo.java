3f1fbfbf176b399b6b7ca35b114fc4790d499e94
pooja <poojamadake73@gmail.com>
2022-12-30 15:09:21 +0530

    ArrayListDemo program is added

---
+++ ArrayListDemo/src/com/demo/ArrayListDemo.java
@@ -0,0 +1,14 @@
+package com.demo;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     List<String> list= Arrays.asList( "ram", "pooja", "onmkar", "shivam");
+     System.out.print(list);
+	}
+
+}
